3adf977e93c19ed4e698c104ad16cbd61899c0d1
John Xander <johnblox5@gmail.com>
2022-05-24 22:12:29 +0100

    fetch random API

--- react-stuff/src/App.js
+++ react-stuff/src/App.js
@@ -1,7 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const fetchRandomData = () => {
+  return axios.get('https://randomuser.me/api')
+  .then(({data}) => {
+    console.log(data);
+    return JSON.stringify(data);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [randomUserDataJSON, setRandomUserDataJSON] = useState("");
+
+  useEffect(() => {
+    fetchRandomData().then(randomData => {
+      setRandomUserDataJSON(randomData || "No user data found.");
+    })
+  }, [])
 
   return (
     <div className="app">
@@ -11,6 +30,7 @@ const App = () => {
         <button onClick={() => {
           setCounter(counter + 1)
         }}>Increase Counter</button>
+        <p className="randomAPI">{randomUserDataJSON}</p>
       </div>
     </div>
   );
